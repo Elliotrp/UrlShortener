83844220c4b9cba14e511f6bfd6d5015c2d6271c
Elliotrp <ellraypres@hotmail.co.uk>
2023-07-23 12:17:10 +0100

    add some choropleth mouse event handling

--- front/src/app/components/pages/analytics/analytics.component.ts
+++ front/src/app/components/pages/analytics/analytics.component.ts
@@ -9,7 +9,7 @@ export class AnalyticsComponent implements OnInit {
    public choroplethData: Map<string, number> = new Map<string, number>();
    
    public ngOnInit(): void {
-      const testData = [{ id: '156', clicks: 10 }, { id: '840', clicks: 5}, { id: '826', clicks: 7}];
+      const testData = [{ id: '156', clicks: 10 }, { id: '840', clicks: 4}, { id: '826', clicks: 3}, { id: '032', clicks: 1}, { id: '180', clicks: 2}];
       testData.forEach(d => {
          this.choroplethData.set(d.id, d.clicks)
       });
--- front/src/app/components/shared/choropleth/choropleth.component.ts
+++ front/src/app/components/shared/choropleth/choropleth.component.ts
@@ -26,14 +26,39 @@ export class ChoroplethComponent implements OnInit {
       if (countries) {
          this.path = d3.geoPath(d3.geoMercator().fitWidth(800, mesh(countries, countries.objects.land)).translate([400, 250]));
          this.svg
-            .append("g")
+            .append('g')
             .attr('class', 'countries')
             .selectAll('path')
             .data(feature(countries, countries.objects.countries).features)
             .enter()
             .append('path')
             .attr('fill', (d: any) => this.colour((d.clicks = this.data.get(d.id))))
-            .attr('d', this.path);
+            .attr('d', this.path)
+            .attr('class', (d: any) => this.data.get(d.id) ? 'has-clicks' : null)
+            .on('click', (d: PointerEvent, i: any) => this.showMessage(i))
+            .on('mouseover', (d: MouseEvent, i: any) => this.mouseOver(d, i))
+            .on('mouseleave', (d: MouseEvent, i: any) => this.mouseLeave(d, i))
+      }
+   }
+
+   private mouseOver = (d: MouseEvent, country: any): void => {
+      if (country.clicks) {
+         d3.selectAll('path').classed('translucent', true)
+         d3.select(d.target as HTMLElement)
+            .classed('translucent', false)
+            .classed('selected', true)
+      }
+   }
+
+   private mouseLeave = (d: MouseEvent, country: any): void => {
+      d3.selectAll('path').classed('translucent', false)
+      d3.select(d.target as HTMLElement)
+         .classed('selected', false)
+   }
+   
+   private showMessage = (country: any): void => {
+      if (country.clicks) {
+         window.alert(country.properties.name + ' has ' + country.clicks + ' clicks.');
       }
    }
 }
