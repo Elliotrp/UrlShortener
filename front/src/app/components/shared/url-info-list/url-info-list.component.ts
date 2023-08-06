3e24b2d4db258bbd32ee48eb3ed34c067a963ae8
Elliotrp <ellraypres@hotmail.co.uk>
2023-08-06 18:14:49 +0100

    small change

--- front/src/app/components/shared/url-info-list/url-info-list.component.ts
+++ front/src/app/components/shared/url-info-list/url-info-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { IUrl } from 'src/app/interfaces/url.interface';
-import { UrlLocalStorageService } from 'src/app/services/url-local-storage/url-local-storage.service';
 
 @Component({
    selector: 'app-url-info-list',
@@ -9,7 +8,4 @@ import { UrlLocalStorageService } from 'src/app/services/url-local-storage/url-l
 })
 export class UrlInfoListComponent {
    @Input() public urls: IUrl[] = [];
-
-   constructor(
-      public readonly urlLocalStorageService: UrlLocalStorageService) { }
 }
