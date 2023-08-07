bce9cc5b94cb707b9ebefb8944bec7a4b32288dc
Elliotrp <ellraypres@hotmail.co.uk>
2023-08-07 18:32:51 +0100

    map browsers

--- front/src/app/services/device/device.service.ts
+++ front/src/app/services/device/device.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { DeviceDetectorService } from 'ngx-device-detector';
+import { DeviceDetectorService, BROWSERS } from 'ngx-device-detector';
 import { IDeviceInfo } from 'src/app/interfaces/device-info.interface';
 
 @Injectable({
@@ -12,10 +12,24 @@ export class DeviceService {
 
    public getDeviceInfo(): IDeviceInfo {
       this.deviceInfo = this.deviceInfo ? this.deviceInfo : {
-         browser: this.deviceDetector.browser,
+         browser: browsers[this.deviceDetector.browser],
          operatingSystem: this.deviceDetector.os,
          deviceType: this.deviceDetector.deviceType
       };
       return this.deviceInfo
    }
+}
+
+const browsers: Record<string, string> = {
+  'Chrome': BROWSERS.CHROME,
+  'Firefox': BROWSERS.FIREFOX,
+  'Safari': BROWSERS.SAFARI,
+  'Opera': BROWSERS.OPERA,
+  'Internet Explorer': BROWSERS.IE,
+  'Edge': BROWSERS.MS_EDGE,
+  'Edge Chromium': BROWSERS.MS_EDGE_CHROMIUM,
+  'Facebook Messenger': BROWSERS.FB_MESSANGER,
+  'Samsung': BROWSERS.SAMSUNG,
+  'UC Browser': BROWSERS.UCBROWSER,
+  'Unknown': BROWSERS.UNKNOWN
 }
